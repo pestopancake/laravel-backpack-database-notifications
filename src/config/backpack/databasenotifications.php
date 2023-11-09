15838b9e19e8ca46f7ffacdbf5e7592992471724
Laszlo Olah <laszlo@dashrocks.com>
2023-11-09 16:12:56 +0200

    Fix sidebar menu item, add ability to provide notification model from config

--- src/config/backpack/databasenotifications.php
+++ src/config/backpack/databasenotifications.php
@@ -4,4 +4,5 @@ return [
     'enable_ajax_count' => true, // refresh the notification shown in the sidebar nav every second
     'enable_toasts' => true, // display a toast notification when a new notification is recieved (requires enable_ajax_count enabled)
     'admin_permission_name' => 'notifications admin', // the permission to check to see if the user should see admin functionality
+    'notification_model' => \Pestopancake\LaravelBackpackNotifications\Models\Notification::class, // the notification class to use
 ];
